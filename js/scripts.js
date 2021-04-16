edd8325abbd2fd518c2b26f5a39e6239fbb38c43
Stewart ilondanga <stilondanga@gmail.com>
2021-04-16 18:39:56 +0300

    add js/scripts.js:

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-/*var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+var navigate = (function() {
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
-
+/*
 var demo = new Vue({
   el: "#demo",
   data: {
